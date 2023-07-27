ef7b4fe6f5a16c025d920ae151d076e8c487967f
ivkram <im.kramarenko@gmail.com>
2023-07-27 21:00:54 +0200

    Create an AstroLabels object using a fixture

--- astrolabel/tests/test_main.py
+++ astrolabel/tests/test_main.py
@@ -1,25 +1,24 @@
 from astrolabel import AstroLabels
+import pytest
 
 
-def test_read_defaults():
-    AstroLabels.read()
+@pytest.fixture(scope="module")
+def al() -> AstroLabels:
+    return AstroLabels.read()
 
 
-def test_without_unit():
-    al = AstroLabels.read()
+def test_without_unit(al):
     assert al.get_label('z') == '$z$'
 
 
-def test_with_unit():
-    al = AstroLabels.read()
+def test_with_unit(al):
     assert al.get_label('sfr') == r'$\mathrm{SFR}$ [$\mathrm{M_{\odot}\,yr^{-1}}$]'
 
 
-def test_log_without_unit():
-    al = AstroLabels.read()
+def test_log_without_unit(al):
     assert al.get_label('z', fmt='log') == r'$\log_{10}\,\left(z\right)$'
 
 
-def test_log_with_unit():
-    al = AstroLabels.read()
-    assert al.get_label('sfr', fmt='log') == r'$\log_{10}\,\left(\mathrm{SFR} / \left(\mathrm{M_{\odot}\,yr^{-1}}\right)\right)$'
+def test_log_with_unit(al):
+    assert al.get_label('sfr', fmt='log') == (r'$\log_{10}\,\left(\mathrm{SFR} / \left(\mathrm{M_{\odot}\,'
+                                              r'yr^{-1}}\right)\right)$')
