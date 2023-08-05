ee6826fb718e3602ba5d9b7e5d4840567a5b9322
ivkram <im.kramarenko@gmail.com>
2023-08-05 14:17:41 +0200

    Test `library_fname()`

--- astrolabel/tests/test_main.py
+++ astrolabel/tests/test_main.py
@@ -4,16 +4,14 @@ import pytest
 import shutil
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def set_env(monkeypatch):
     monkeypatch.delenv("ASTROLABEL", raising=False)
 
 
-@pytest.fixture
-def ll() -> LabelLibrary:
-    ll = LabelLibrary.read()
-    assert ll.library_fname() == DEFAULT_LIBRARY_PATH
-
+@pytest.fixture(scope="module")
+def std_ll() -> LabelLibrary:
+    ll = LabelLibrary.read(DEFAULT_LIBRARY_PATH)
     return ll
 
 
@@ -23,6 +21,13 @@ def tmp_library_path(tmp_path):
     shutil.copy(DEFAULT_LIBRARY_PATH, library_path)
 
     return library_path
+
+
+def test_library_fname(std_ll):
+    assert std_ll.library_fname() == DEFAULT_LIBRARY_PATH
+
+    ll = LabelLibrary(formats={}, labels={})
+    assert ll.library_fname() is None
 
 
 def test_info(capsys):
@@ -40,37 +45,43 @@ def test_read_from_file(tmp_library_path):
     assert ll.library_fname() == tmp_library_path
 
 
-def test_read_from_env_variable(tmp_library_path, monkeypatch):
-    monkeypatch.setenv("ASTROLABEL", str(tmp_library_path))
-    ll = LabelLibrary.read()
-
-    assert ll.library_fname() == tmp_library_path
-
-
-def test_read_from_workdir(tmp_library_path, monkeypatch):
+def test_read_from_workdir(set_env, tmp_library_path, monkeypatch):
     monkeypatch.chdir(tmp_library_path.parent)
     ll = LabelLibrary.read()
 
     assert ll.library_fname() == tmp_library_path
 
 
-def test_read_not_found():
+def test_read_from_env_variable(set_env, tmp_library_path, monkeypatch):
+    monkeypatch.setenv("ASTROLABEL", str(tmp_library_path))
+    ll = LabelLibrary.read()
+
+    assert ll.library_fname() == tmp_library_path
+
+
+def test_read_from_default_location(set_env):
+    ll = LabelLibrary.read()
+
+    assert ll.library_fname() == DEFAULT_LIBRARY_PATH
+
+
+def test_read_file_not_found():
     with pytest.raises(FileNotFoundError, match="File \".*\" does not exist"):
         LabelLibrary.read("non_existing_file.yml")
 
 
-def test_without_unit(ll):
-    assert ll.get_label('z') == '$z$'
+def test_get_label_without_unit(std_ll):
+    assert std_ll.get_label('z') == '$z$'
 
 
-def test_with_unit(ll):
-    assert ll.get_label('sfr') == r'$\mathrm{SFR}$ [$\mathrm{M_{\odot}\,yr^{-1}}$]'
+def test_get_label_with_unit(std_ll):
+    assert std_ll.get_label('sfr') == r'$\mathrm{SFR}$ [$\mathrm{M_{\odot}\,yr^{-1}}$]'
 
 
-def test_log_without_unit(ll):
-    assert ll.get_label('z', fmt='log') == r'$\log_{10}\,z$'
+def test_get_label_log_without_unit(std_ll):
+    assert std_ll.get_label('z', fmt='log') == r'$\log_{10}\,z$'
 
 
-def test_log_with_unit(ll):
-    assert ll.get_label('sfr', fmt='log') == (r'$\log_{10}\,\left(\mathrm{SFR} / \mathrm{M_{\odot}\,'
+def test_get_label_log_with_unit(std_ll):
+    assert std_ll.get_label('sfr', fmt='log') == (r'$\log_{10}\,\left(\mathrm{SFR} / \mathrm{M_{\odot}\,'
                                               r'yr^{-1}}\right)$')
